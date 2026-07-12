153ab57aa061224a107babec24f016cb9166f036
yuval06082000-collab <yuval06082000@gmail.com>
2026-07-12 16:36:08 +0300

    Update student ID in API request payload

--- script9.py
+++ script9.py
@@ -1,4 +1,4 @@
 import requests
 url = "https://server.iac.ac.il/api/v1/studentapi/generate_key"
-payload = {"id": "322631961", "password": "Maho8135"}
+payload = {"id": "123456789", "password": "Maho8135"}
 print(requests.post(url, json=payload).json())
